2eb072aa7c2664e6cc74ddfa353124d4d4c2652f
Jeong Pak <dannypak716@gmail.com>
2021-12-03 12:37:38 -0500

    actions get by id works and can post new actions if proper arguments are passed in.  working on PUT right now

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,57 @@
 const Action = require('./actions-model')
+
+async function validateActionId(req, res, next) {
+    try {
+      const action = await Action.get(req.params.id);
+      if (!action) {
+        next({
+          status: 404,
+          message: 'project not found'
+        })
+      } else {
+        req.action = action
+        next()
+      }
+    } catch(err) {
+      next(err);
+    }
+}
+
+function validateAction(req, res, next) {
+    try {
+        if(!req.body.project_id || !req.body.description || !req.body.notes) {
+            next({
+                status: 400,
+                message: 'action requires project_id, description, and notes'
+            })
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+function validateActionUpdate(req, res, next) {
+    try {
+        if(req.body.project_id && 
+            req.body.name && 
+            req.body.description && 
+            req.body.completed !== undefined) {
+            next()
+        } else {
+            next({
+              status: 400,
+              message: 'requires a project_id, description, notes, and completed status'
+          })
+        }
+    } catch (err) {
+        next(err)
+    }
+  }
+
+module.exports = {
+    validateActionId,
+    validateAction,
+    validateActionUpdate
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const {
+    validateActionId,
+    validateAction,
+    validateActionUpdate
+} = require('./actions-middlware')
 
 const Action = require('./actions-model')
 
@@ -12,21 +17,47 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
-router.get('/', (req, res) => {
-    
+router.get('/:id', validateActionId, (req, res) => {
+    res.json(req.action)
 })
 
-// router.get('/:id', (req, res) => {
-    
-// })
+router.post('/', validateAction, async (req, res, next) => {
+    try{
+        const newAction = await Action.insert({
+            project_id: req.body.project_id,
+            description: req.body.description,
+            notes: req.body.notes,
+            completed: req.body.completed || false
+        })
+        res.status(201).json(newAction)
+    } catch(err) {
+        next(err)
+    }
+})
 
-// router.post('/', (req, res) => {
-    
-// })
+router.put('/:id', validateActionId, validateActionUpdate, async (req, res, next) => {
+    try{
 
-// router.put('/:id', (req, res) => {
-    
-// })
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', validateProjectId, validateProjectUpdate, async (req, res, next) => {
+    try{
+        const { id } = req.params
+        const { name, description, completed } = req.body
+        const updatedProject = await Project.update(id, {
+            name: name,
+            description: description,
+            completed: completed 
+        })
+        console.log(updatedProject)
+        res.status(200).json(updatedProject)
+    } catch (err) {
+        next(err)
+    }
+})
 
 // router.delete('/:id', (req, res) => {
     
